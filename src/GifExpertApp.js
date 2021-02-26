06e92a4407154c369321d414078ca0c2df037661
Pamela Suarez <pamela.suarez.roldan@gmail.com>
2021-02-25 19:03:06 -0600

    first commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+    
+export const GifExpertApp = () => {
+    // const categories = ['Hello Kitty', 'Snoopy', 'Avengers'];
+   
+    const [categories, setCategories] = useState( ['Hello Kitty']);
+    const handleAdd= (e, cat) => {
+        setCategories([...categories,cat]);
+    }
+    return (
+        <div>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories = {setCategories} />
+            <hr></hr>
+           
+            <ol>
+                {
+                    categories.map(category => (
+                        <GifGrid 
+                        key = {category}
+                        category ={category}></GifGrid>
+
+                    ))
+                    
+                }
+
+            </ol>
+        
+
+        </div>
+    )
+}
+
